0c21e4820ddcafc7db9f4f667fe0485946fb817b
sum-it-up-9 <sumitadsul9@gmail.com>
2023-09-21 17:40:04 +0530

    inital backend commit

--- client/src/components/About.js
+++
@@ -1,9 +0,0 @@
-const About= () =>{
-    return (
-        <>
-            <h1>This is About Us Page</h1>
-        </>
-    )
-};
-
-export default About;
--- client/src/components/Contact.js
+++
@@ -1,9 +0,0 @@
-const Contact=()=>{
-    return (
-        <>
-            <h1>Cotact Us page</h1>
-        </>
-    )
-}
-
-export default Contact;
